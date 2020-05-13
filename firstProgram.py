b0b9ebff5c4609b188b40c3214297e7e14b9889b
Tassia Barros <tassinha.cpr@gmail.com>
2020-05-13 19:27:13 -0300

    Primeiro programa
    Add do arquivo com o código do primeiro programa do módulo 2

---
+++ firstProgram.py
@@ -0,0 +1,27 @@
+print("Hello, World!")
+
+print("My name is", "Python.")
+print("Monty Python.")
+
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+print("My", "name", "is", "Monty", "Python.", sep="-")
+print("My", "name", "is", "Monty", "Python.", sep=" ")
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programming","Essentials","in")
+print("Python")
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+
+print("    *", end="\n")
+print("   * *", end="\n")
+print("  *   *", end="\n")
+print(" *     *", end="\n")
+print("***   ***", end="\n")
+print("  *   *", end="\n")
+print("  *   *", end="\n")
+print("  *****", end="\n")
