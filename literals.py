345e521326bef7db65357279298b9aab91e833f3
Tassia Barros <tassinha.cpr@gmail.com>
2020-05-13 19:29:16 -0300

    Literais
    Add do arquivo com um exemplo de aplicação de literais

---
+++ literals.py
@@ -0,0 +1,19 @@
+# integer: hexa or octal representation
+print(0o123)
+print(0x123)
+
+# floats
+print(0.0000000000000000000001)
+
+# string
+print("I like \"Monty Python\"")
+print('I like "Monty Python"')
+print('I\'m Monty Python.')
+print("I'm Monty Python.")
+
+# boolean
+print(True > False)
+print(True < False)
+
+# LAB
+print('"I\'m"', '\n""learning""', '\n"""Python"""')
